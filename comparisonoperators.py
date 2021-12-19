6902121de8f5dcf48269604f56f085c7a826fcba
TAMANNA1JAIN <95642357+TAMANNA1JAIN@users.noreply.github.com>
2021-12-19 21:03:47 +0530

    Add files via upload

---
+++ comparisonoperators.py
@@ -0,0 +1,9 @@
+#comparision operator
+x=7
+y=5
+print(x!=y) 
+
+x=6
+y=5
+print(x>=y)
+
---
+++ logicaloperator.py
@@ -0,0 +1,7 @@
+#logical operator 
+x=True 
+y=False
+print(x and y)
+print(x or y)
+print(x is y)
+print(x is not y)
